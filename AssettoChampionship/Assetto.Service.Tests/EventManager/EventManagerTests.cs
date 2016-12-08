a266c1c6abe86e7e27a22b0587d8049b0b7f8d63
jozsinakhivnak <hj1025@hszk.bme.hu>
2016-12-08 12:38:22 +0100

    improvements

--- AssettoChampionship/Assetto.Service.Tests/EventManager/EventManagerTests.cs
+++ AssettoChampionship/Assetto.Service.Tests/EventManager/EventManagerTests.cs
@@ -94,5 +94,76 @@ namespace Assetto.Service.Tests.EventManager
                 SupportedEvents.Abarth500RaceEvent1,
                 SupportedSessions.Abarth500RaceEvent1Race1);
         }
+
+        [TestMethod]
+        public void EventManager_EmptyPreviousResult_SholdNotInvokeLoadResult()
+        {
+            var seriesServiceMock = new Mock<ISeriesService>();
+            var processServiceMock = new Mock<IProcessService>();
+            var saveService = new Mock<ISaveService>();
+            var resultServiceMock = new Mock<IResultService>();
+            var eventServiceMock = new Mock<IEventService>();
+
+
+            // FileService
+            FileServiceMock
+             .Setup(m => m.ReadFile(It.IsAny<string>()))
+             .Returns(string.Empty);
+
+
+            // SeriesService
+
+            // ProcessService
+            processServiceMock
+                .Setup(m => m.StartProcess(It.IsAny<string>()))
+                .Verifiable();
+
+            processServiceMock
+                .Setup(m => m.MonitorProcess(It.IsAny<string>(),
+                    It.IsAny<ProcessInfo.StartedEventHandler>(),
+                    It.IsAny<ProcessInfo.TerminatedEventHandler>()))
+                .Verifiable();
+
+            // SaveService
+            saveService
+                .Setup(m => m.LoadResult(It.IsAny<Guid>(),
+                    It.IsAny<Guid>(),
+                    It.IsAny<Guid>()))
+                .Throws(new Exception()) // Should not invoke this method
+                .Verifiable();
+
+            saveService
+                .Setup(m => m.SaveResult(
+                    It.IsAny<Guid>(),
+                    It.IsAny<Guid>(),
+                    It.IsAny<Guid>(),
+                    It.IsAny<Result>()
+                ))
+                .Returns(new SavedSeason());
+
+            // ResultService
+            resultServiceMock
+                .Setup(m => m.GetResultForLog(It.IsAny<string>()))
+                .Returns(new Result());
+
+            // EventService
+            eventServiceMock.Setup(m => m.OrderGrid(It.IsAny<ConfiguredSessionDTO>())).Verifiable();
+
+            var eventManager = new Manager.EventManager(
+                this.FileServiceMock.Object,
+                seriesServiceMock.Object,
+                processServiceMock.Object,
+                resultServiceMock.Object,
+                saveService.Object,
+                this.ConfigServiceMock.Object,
+                eventServiceMock.Object
+                );
+
+
+            eventManager.StartEvent(SupportedSeries.AbarthRaceSeries,
+                    SupportedEvents.Abarth500RaceEvent1,
+                    SupportedSessions.Abarth500RaceEvent1Qualy);
+
+        }
     }
 }
