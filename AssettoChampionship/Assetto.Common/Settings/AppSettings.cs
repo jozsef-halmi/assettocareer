87976b112d0dc83ecb3f0ecadabafc79abcef283
jozsinakhivnak <hj1025@hszk.bme.hu>
2016-12-14 13:43:04 +0100

    improvements

---
+++ AssettoChampionship/Assetto.Common/Settings/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assetto.Common.Settings
+{
+    public class AppSettings
+    {
+        string DocumentsFolder { get; set; }
+        string RaceIniRelativePathToDocFolder { get; set; }
+        string OutputLogRelativePathToDocFolder { get; set; }
+        string AssettoCorsaInstallLoc { get; set; }
+        string AssettoCorsaExeRelativePathToACFolder { get; set; }
+    }
+}
