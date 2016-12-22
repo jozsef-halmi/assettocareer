0093ed5336fe6d63a5eabb75b9d95558367734e1
jozsinakhivnak <hj1025@hszk.bme.hu>
2016-12-22 12:35:31 +0100

    improvements

---
+++ AssettoChampionship/AssettoChampionship/ViewModels/NextSessionViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Assetto.Common.Data;
+using Assetto.Common.DTO;
+using Assetto.Common.Framework;
+using Assetto.Common.Interfaces.Manager;
+using Caliburn.Micro;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AssettoChampionship.ViewModels
+{
+    public class NextSessionViewModel : Screen
+    {
+        // Managers
+        public IEventAggregator EventAggregator { get; set; }
+        public IEventManager EventManager { get; set; }
+
+        public NextSessionViewModel(
+            IEventAggregator eventAggregator
+            , IEventManager eventManager)
+        {
+            this.EventAggregator = eventAggregator;
+            this.EventManager = eventManager;
+
+
+        }
+
+        protected override void OnActivate()
+        {
+            base.OnActivate();
+        }
+    }
+}
