f796d1d7a27f13e291af35193617c20d3c8f7a1e
jozsinakhivnak <hj1025@hszk.bme.hu>
2016-12-22 10:29:45 +0100

    improvements

---
+++ AssettoChampionship/Assetto.Common/Data/PathData.cs
@@ -0,0 +1,26 @@
+﻿using Assetto.Common.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assetto.Common.Data
+{
+    public class PathData
+    {
+        public string Name { get; set; }
+        public string FriendlyName { get; set; }
+
+        public string ImageUrl
+        {
+            get { return "/Images/Paths/" + Name + ".jpg"; }
+        }
+
+        public string Description { get; set; }
+        public string VideoUrl { get; set; }
+
+        public List<ClassPathData> PathClasses { get; set; }
+        public Guid PathId { get; set; }
+    }
+}
